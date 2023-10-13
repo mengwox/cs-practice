f57588f23de02a5be8e34b085466f7b149929238
mengwox <mwh8699329@163.com>
2023-10-13 16:06:02 +0800

    code reformat

--- leetcode/src/main/java/easy/RomanToInt.java
+++ leetcode/src/main/java/easy/RomanToInt.java
@@ -1,39 +1,44 @@
 package easy;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * 13.罗马数字转整数
  */
 public class RomanToInt {
 
-    public int romanToInt(String s) {
-        Map<Character, Integer> container = container();
-        int len = s.length();
-        int result = 0;
-        int preInt = 0;
-        for (int i = len - 1; i >= 0; i--) {
-            Integer curInt = container.get(s.charAt(i));
-            if (preInt > curInt) {
-                result -= curInt;
-            } else {
-                result += curInt;
-            }
-            preInt = curInt;
-        }
-        return result;
-    }
+	public int romanToInt(String s) {
+		int len = s.length();
+		int result = 0;
+		int preInt = 0;
+		for (int i = len - 1; i >= 0; i--) {
+			int curInt = getValue(s.charAt(i));
+			if (preInt > curInt) {
+				result -= curInt;
+			} else {
+				result += curInt;
+			}
+			preInt = curInt;
+		}
+		return result;
+	}
 
-    private Map<Character, Integer> container() {
-        Map<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
-        return map;
-    }
+	private int getValue(char c) {
+		switch (c) {
+			case 'I':
+				return 1;
+			case 'V':
+				return 5;
+			case 'X':
+				return 10;
+			case 'L':
+				return 50;
+			case 'C':
+				return 100;
+			case 'D':
+				return 500;
+			case 'M':
+				return 1000;
+			default:
+				return 0;
+		}
+	}
 }
