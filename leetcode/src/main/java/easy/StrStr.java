3ba589eacd40dafdc7af834185378c42b5917841
mengwox <mwh8699329@163.com>
2023-10-17 00:52:49 +0800

    LeetCode 28 commit

--- leetcode/src/main/java/easy/StrStr.java
+++ leetcode/src/main/java/easy/StrStr.java
@@ -4,9 +4,15 @@ package easy;
  * <p>28.找出字符串中第一个匹配项的下标</p>
  */
 public class StrStr {
-
 	public int strStr(String haystack, String needle) {
-		int slow = 0;
-		return slow == haystack.length() ? -1 : slow;
+		int index = 0;
+		for (int i = 0; i < haystack.length(); i++) {
+			if (haystack.charAt(i) != needle.charAt(i - index)) {
+				i = index++;
+			} else if (i - index == needle.length() - 1) {
+				return index;
+			}
+		}
+		return -1;
 	}
 }
---
+++ leetcode/src/test/java/easy/StrStrTest.java
@@ -0,0 +1,15 @@
+package easy;
+
+import org.junit.jupiter.api.Test;
+
+class StrStrTest {
+	private StrStr solution = new StrStr();
+
+	@Test
+	void strStr() {
+		assert 0 == solution.strStr("leetcode", "leet");
+		assert -1 == solution.strStr("leetcode", "leeto");
+		assert 4 == solution.strStr("mississippi", "issip");
+		assert 2 == solution.strStr("hello", "ll");
+	}
+}
