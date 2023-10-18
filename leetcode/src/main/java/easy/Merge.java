2aca1779536c39653cefd326ea00f4c2fe86f146
mengwox <mwh8699329@163.com>
2023-10-18 10:14:41 +0800

    commit leetCode No.88

--- leetcode/src/main/java/easy/Merge.java
+++ leetcode/src/main/java/easy/Merge.java
@@ -1,7 +1,35 @@
 package easy;
 
+/**
+ * <p>88.合并两个有序数组</p>
+ */
 public class Merge {
 	public void merge(int[] nums1, int m, int[] nums2, int n) {
+		int i1 = m - 1;
+		int i2 = n - 1;
+		while (i1 >= 0 && i2 > 0) {
+			if (nums1[i1] > nums2[i2]) {
+				i1--;
+			} else {
+				int index = m + n - 2;
+				while (index > i1) {
+					nums1[index + 1] = nums1[index];
+					index--;
+				}
+				nums1[i1 + 1] = nums2[i2--];
+			}
+		}
 
+		if (i2 >= 0) {
+			int index = m + n - 1;
+			while (index >= 0) {
+				if (index > i2) {
+					nums1[index] = nums1[index - i2 - 1];
+				} else {
+					nums1[index] = nums2[index];
+				}
+				index--;
+			}
+		}
 	}
 }
--- leetcode/src/test/java/easy/MergeTest.java
+++ leetcode/src/test/java/easy/MergeTest.java
@@ -2,12 +2,12 @@ package easy;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MergeTest {
 	private final Merge solution = new Merge();
 
 	@Test
 	void merge() {
+		solution.merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3);
+		solution.merge(new int[]{4, 5, 6, 0, 0, 0}, 3, new int[]{1, 2, 3}, 3);
 	}
 }
