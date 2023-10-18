7ca74bab294da9e2444f6d6393c65cb37bbd8ea4
mengwox <mwh8699329@163.com>
2023-10-18 10:49:18 +0800

    LeetCode No.125 commit

---
+++ leetcode/src/main/java/easy/IsPalindrome.java
@@ -0,0 +1,43 @@
+package easy;
+
+/**
+ * 125.验证回文串
+ */
+public class IsPalindrome {
+	public boolean isPalindrome(String s) {
+		int start = 0;
+		int end = s.length() - 1;
+		while (start <= end) {
+			int sc = s.charAt(start);
+			int ec = s.charAt(end);
+			if (isNotValid(sc)) {
+				start++;
+				continue;
+			}
+			if (isNotValid(ec)) {
+				end--;
+				continue;
+			}
+			if (isUpperWord(sc)) {
+				sc = sc + 32;
+			}
+			if (isUpperWord(ec)) {
+				ec = ec + 32;
+			}
+			if (sc != ec) {
+				return false;
+			}
+			start++;
+			end--;
+		}
+		return true;
+	}
+
+	private boolean isNotValid(int word) {
+		return (word < 65 || word > 90) && (word < 97 || word > 122) && (word < 48 || word > 57);
+	}
+
+	private boolean isUpperWord(int word) {
+		return word >= 65 && word <= 90;
+	}
+}
---
+++ leetcode/src/test/java/easy/IsPalindromeTest.java
@@ -0,0 +1,19 @@
+package easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class IsPalindromeTest {
+	private final IsPalindrome solution = new IsPalindrome();
+
+	@Test
+	void isPalindrome() {
+		assertTrue(solution.isPalindrome("ab_a"));
+		assertTrue(solution.isPalindrome("A man, a plan, a canal: Panama"));
+		assertTrue(solution.isPalindrome(" "));
+		assertFalse(solution.isPalindrome("race a car"));
+		assertFalse(solution.isPalindrome("0P"));
+	}
+}
