7dddf5ec5f6993e65e67795cfee922bcb7d385ed
mengwox <mwh8699329@163.com>
2023-12-01 18:08:33 +0800

    Create IsSameTree.java

---
+++ leetcode/src/main/java/easy/IsSameTree.java
@@ -0,0 +1,22 @@
+package easy;
+
+import easy.inner.TreeNode;
+
+public class IsSameTree {
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		if (p == null && q == null) {
+			return true;
+		}
+		if (p == null || q == null) {
+			return false;
+		}
+		if (p.val != q.val) {
+			return false;
+		}
+		boolean res = isSameTree(p.left, q.left);
+		if (!res) {
+			return false;
+		}
+		return isSameTree(p.right, q.right);
+	}
+}
