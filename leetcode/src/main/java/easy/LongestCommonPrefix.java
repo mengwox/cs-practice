bb3085a68c1f13fee397518a1278374855ad3b94
mengwox <mwh8699329@163.com>
2024-03-01 17:29:30 +0800

    Update LongestCommonPrefix.java

--- leetcode/src/main/java/easy/LongestCommonPrefix.java
+++ leetcode/src/main/java/easy/LongestCommonPrefix.java
@@ -4,28 +4,31 @@ package easy;
  * 14.最长公共前缀
  */
 public class LongestCommonPrefix {
-	public String longestCommonPrefix(String[] strs) {
-		//边界
-		String item = strs[0];
-		if (strs.length == 1) {
-			return item;
-		}
-		int len = item.length();
-		int count = 0;
-		for (int i = 0; i < len; i++) {
-			int c = 0;
-			for (String str : strs) {
-				if (i < str.length() && item.charAt(i) == str.charAt(i)) {
-					c++;
-				}
-			}
-			if (c != strs.length) {
-				//并不是所有元素都相同
-				break;
-			}
-			//所有元素index=i的char都相同
-			count++;
-		}
-		return item.substring(0, count);
-	}
+    public String longestCommonPrefix(String[] strs) {
+        String result = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            String tmp = getCommonPrefix(result, strs[i]);
+            if (tmp.length() < result.length()) {
+                if (tmp.isEmpty()) {
+                    return tmp;
+                }
+                result = tmp;
+            }
+        }
+        return result;
+    }
+
+    private String getCommonPrefix(String str1, String str2) {
+        int size = Math.min(str1.length(), str2.length());
+        if (size == 0) {
+            return "";
+        }
+        int i;
+        for (i = 0; i < size; i++) {
+            if (str1.charAt(i) != str2.charAt(i)) {
+                break;
+            }
+        }
+        return str1.substring(0, i);
+    }
 }
